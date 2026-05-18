e1c817309a6a0e7a3986102879ed81a12c117f67
Nitesh-GPT <ng4433365@gmail.com>
2026-05-18 17:50:06 +0530

    find the min

--- Q2.js
+++ Q2.js
@@ -1,3 +1,14 @@
 // find the min number in the given array.
 
-let numbers = [1,7,6,9,3,2,1,5,]
+let numbers = [1,7,6,9,3,2,1,5,];
+
+let Minvalue = numbers[0];
+
+for (let i=1; i<numbers.length; i++){
+    if (numbers[i] < Minvalue){
+
+       Minvalue = numbers[i];
+
+    }
+}
+console.log(Minvalue); 
