20ce6e54dd41e97518ad640c186443d378c87c20
Nitesh-GPT <ng4433365@gmail.com>
2026-05-18 17:53:33 +0530

    Count all the prime numbers of given array

---
+++ Q5.js
@@ -0,0 +1,11 @@
+// count all the prime numbers
+
+let array = [1,2,3,4,5,6,7,9];
+let sum = 0;
+
+for (i=0; i<array.length; i++){
+    if (array[i]%2 === 1){
+        sum++
+    }
+} 
+console.log(sum);
