fbceec662296ac9a9d590d6467e7e0d1c8680e81
Nitesh-GPT <ng4433365@gmail.com>
2026-05-19 19:58:47 +0530

    Reverse the array using while loop

---
+++ Q9.js
@@ -0,0 +1,10 @@
+// Reverse the array using While loop?
+let array = [1,2,3,4,5,6,7,8,9];
+let reverxed = [];
+let i = array.length - 1;
+
+while (i>0){
+    reverxed.push(i);
+    i--;
+}
+console.log(reverxed);
