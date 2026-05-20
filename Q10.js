08ea246dc3e794f32e6d8be603df22fc7d304dc6
Nitesh-GPT <ng4433365@gmail.com>
2026-05-20 19:52:22 +0530

    Fibonacci Sequence printing!

---
+++ Q10.js
@@ -0,0 +1,20 @@
+// Fibonacci sequence
+
+let n = 7;
+
+let a = 0;
+let b = 1;
+
+console.log(a);
+console.log(b);
+
+for(let i = 2; i < n; i++) {
+
+   let c = a + b;
+
+   console.log(c);
+
+   a = b;
+   b = c;
+}
+
