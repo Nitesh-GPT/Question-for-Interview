465302faf5f6ee3b22bb92642b1f9d163d9a3e9d
Nitesh-GPT <ng4433365@gmail.com>
2026-06-04 13:57:14 +0530

    right triangle

---
+++ Q27.js
@@ -0,0 +1,13 @@
+let row=5;
+for(let i=1; i<=row; i++){
+    let star="";
+    for(let j=1; j<=row; j++){
+        star=" ";
+        
+    }
+    for(let k=1; k<=i; k++){
+        star+="*  ";
+        
+    }
+    console.log(star);
+}
--- Q9.js
+++ Q9.js
@@ -1,7 +1,7 @@
 // Reverse the array using While loop?
 let array = [1,2,3,4,5,6,7,8,9];
 let reverxed = [];
-let i = array.length - 1;
+let i = array.length;
 
 while (i>0){
     reverxed.push(i);
