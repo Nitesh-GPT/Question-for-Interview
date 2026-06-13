6bace97b31cd6a016e42e9fa3ef27fb7fe09392f
Nitesh-GPT <ng4433365@gmail.com>
2026-06-13 16:10:46 +0530

    Find the intersection in arrays

--- Q31.js
+++ Q31.js
@@ -1,15 +1,16 @@
-//find the union of the array
-let a=[1,2,3,4,5];
-let b=[1,2,3];
-for(let i=0; i<a.length; i++){
-    let arr=[];
-    for(let z=0; z<b.length; z++){
-        if(a[i]===b[z]){
-            arr=a[i];
-
+//find the intersection
+let arr = [1,2,3,4,5,6];
+let arr1 = [2,3,4];
+let arr2 = [];
+for(let i=0; i<arr.length; i++){
+    for(let u=0; u<arr1.length; u++){
+        if(arr[i]==arr1[u]){
+            arr2.push(arr[i]);
+            
         }
-        console.log(arr);
     }
+    
+}
+console.log(arr2);
 
-
-};
+//op = [2,3,4]
